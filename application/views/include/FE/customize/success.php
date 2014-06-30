b730132062e418f134428af2b03c90c5ddc02588
chhingchhing <chhingchhing.hem@gmail.com>
2014-06-30 16:17:03 +0700

    condition on show page success on booking customize

--- application/views/include/FE/customize/success.php
+++ application/views/include/FE/customize/success.php
@@ -4,10 +4,20 @@
     <div id="pay_feedback"></div>
     
     <div>
-        <p>Your booking was successfully, thanks you.</p>
-        <p>We will send you the email about detail information for login. This is the url for login into your profile page for adding the more passengers via your amount of people you have entered. </p>
-        <p>Please help us provide information about more passengers by clicking "Add more passengers"</p>
-        <p>This is the url for <?php echo anchor(base_url().$this->uri->segment('1').'/fe_login/loginuser', 'Login'); ?></p>
+        <?php 
+        if ($this->session->userdata('passengerid')) { ?>
+            <p>Your booking was successfully, thanks you.</p>
+            <p>Please help us provide information about more passengers by clicking "Add more passengers"</p>
+            <p>Please see detail of booking in your <?php echo anchor(base_url().$this->uri->segment('1').'/site/profile', 'profile'); ?></p>
+        <?php
+        } else { ?>
+            <p>Your booking was successfully, thanks you.</p>
+            <p>We will send you the email about detail information for login. This is the url for login into your profile page for adding the more passengers via your amount of people you have entered. </p>
+            <p>Please help us provide information about more passengers by clicking "Add more passengers"</p>
+            <p>This is the url for <?php echo anchor(base_url().$this->uri->segment('1').'/fe_login/loginuser', 'Login'); ?></p>
+        <?php }
+        ?>
+        
     </div>
     
 <p></p>
